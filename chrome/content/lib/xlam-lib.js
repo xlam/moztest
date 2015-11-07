baa48db69eb17130c05d5e5e65879aa2f7c8d8b7
Sergey <xlamserg@gmail.com>
2015-11-07 16:17:36 +0400

    [xlam-lib.js] исправление замечаний IDE

--- chrome/content/lib/xlam-lib.js
+++ chrome/content/lib/xlam-lib.js
@@ -1,7 +1,7 @@
-if ("undefined" == typeof XULXlamLib) {
-	var XULXlamLib = {};
+if ("undefined" === typeof XULXlamLib) {
+    var XULXlamLib = {};
 }
 
-XULXlamLib.test = function() {
-	alert("test();");
+XULXlamLib.test = function () {
+    alert("test();");
 };
